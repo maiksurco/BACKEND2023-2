98dd3318d4ad0e4bf1d03386a29dd69bc337c1d2
maiksurco <64235930+maiksurco@users.noreply.github.com>
2023-09-04 10:51:00 -0500

    proudcto

--- maven-producto/src/main/java/maik/example/mavenproducto/MavenProductoApplication.java
+++ maven-producto/src/main/java/maik/example/mavenproducto/MavenProductoApplication.java
@@ -1,7 +1,11 @@
 package maik.example.mavenproducto;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MavenProductoApplication {
@@ -10,4 +14,14 @@ public class MavenProductoApplication {
 		SpringApplication.run(MavenProductoApplication.class, args);
 	}
 
+@Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI().info(new Info()
+                .title("OPEN API MICROSERVICIO CATALOGO")
+                .version("0.0.1")
+                .description("servicios web catálogo")
+                .termsOfService("http://swagger.io/terms")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+        );
+    }
 }
